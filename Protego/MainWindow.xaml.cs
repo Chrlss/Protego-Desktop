41aaac935e89158d3de896a6c5df9f117c9d5ed3
johncarlosuyo <133126608+cjsuyo30@users.noreply.github.com>
2024-03-12 00:37:45 +0800

    3/12 - tapos na birthday mo charles

--- Protego/MainWindow.xaml.cs
+++ Protego/MainWindow.xaml.cs
@@ -78,6 +78,9 @@ namespace Protego
             logIn.Show();
         }
 
-        
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
     }
 }
