d1d6babfeaf20f40e678935a42b3fffe15cd9bc2
johncarlosuyo <133126608+cjsuyo30@users.noreply.github.com>
2024-03-14 16:31:28 +0800

    3/14-Task of cessy

--- Protego/Pages/Monitor.xaml.cs
+++ Protego/Pages/Monitor.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management;
 
 namespace Protego.Pages
 {
@@ -20,9 +21,81 @@ namespace Protego.Pages
     /// </summary>
     public partial class Monitor : Page
     {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
         public Monitor()
         {
             InitializeComponent();
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0);
+            timer.Start();
+
+            GetOSInfo();
+        }
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            GetTime();
+        }
+        private void GetTime()
+        {
+            DateTime date;
+            date = DateTime.Now;
+            LblClock.Text = date.ToLongDateString() + "   " + date.ToLongTimeString();
+        }
+        private void GetOSInfo()
+        {
+            System.Management.ManagementClass wmi = new System.Management.ManagementClass("Win32_Processor");
+            var providers = wmi.GetInstances();
+
+            foreach (var provider in providers)
+            {
+                string procStatus = provider["Status"].ToString();
+                int procFamily = Convert.ToInt16(provider["Family"]);
+
+                LblStatus.Text = "Processor Status" + " " + procStatus.ToString();
+
+                if (procFamily == 107)
+                {
+                    LblSku.Text = "AMD Ryzen 5 5600G";
+                }
+
+                if (procFamily == 11)
+                {
+                    LblSku.Text = "Pentium(R) brand ";
+                }
+
+                if (procFamily == 12)
+                {
+                    LblSku.Text = "Pentium(R) Pro ";
+                }
+
+                if (procFamily == 13)
+                {
+                    LblSku.Text = "Pentium(R) II ";
+
+                }
+
+                if (procFamily == 14)
+                {
+                    LblSku.Text = "Pentium(R) processor with MMX(TM) technology";
+                }
+
+                if (procFamily == 14)
+                {
+                    LblSku.Text = "Pentium(R) processor with MMX(TM) technology";
+                }
+
+                if (procFamily == 15)
+                {
+                    LblSku.Text = "Celeron(TM)";
+                }
+
+                if (procFamily == 16)
+                {
+                    LblSku.Text = "Pentium(R) II Xeon(TM) ";
+                }
+            }
         }
     }
 }
