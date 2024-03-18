a6b9befeed0bcebd16133378a0f727eb15490450
Chrlss <Chrlss@yahoo.com>
2024-03-19 01:15:57 +0800

    3/19

--- Protego/Pages/Home.xaml.cs
+++ Protego/Pages/Home.xaml.cs
@@ -27,8 +27,8 @@ namespace Protego.Pages
 
 
             //GetOSInfo();
-            ProcessorFamily();
-            GetTotalRam();
+            Task.Run(() => ProcessorFamily());
+            
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
@@ -43,32 +43,7 @@ namespace Protego.Pages
             LogInWindow logIn = new LogInWindow();
             logIn.Show();
         }
-        private void GetTotalRam()
-        {
-            try
-            {
-                // Get total RAM using WMI
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\cimv2", "SELECT Capacity FROM Win32_PhysicalMemory");
-                ulong totalRam = 0;
-                foreach (ManagementObject queryObj in searcher.Get())
-                {
-                    totalRam += (ulong)queryObj["Capacity"];
-                }
-
-                // Convert to GB for display (cast to double for accurate division)
-                double ramInGb = Math.Round((double)totalRam / (1024 * 1024 * 1024), 2);
-
-                // Update progress bar value (assuming maximum RAM is 16 GB)
-                ramProgressBar.Maximum = 16384; // Adjust based on your expected maximum RAM
-                ramProgressBar.Value = ramInGb;
-
-                ramTextBlock.Text = $"Total RAM: {ramInGb} GB";
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error retrieving RAM information: {ex.Message}");
-            }
-        }
+       
 
 
         /*
@@ -116,6 +91,6 @@ namespace Protego.Pages
 
         }
 
-       
+        
     }
 }
