339cb2758f253bf57cd6c686d8be08f533b0b676
johncarlosuyo <cjsuyo.30@gmail.com>
2024-04-28 03:11:07 +0800

    home redesign

--- Protego/Pages/Home.xaml.cs
+++ Protego/Pages/Home.xaml.cs
@@ -21,69 +21,9 @@ namespace Protego.Pages
         public Home()
         {
             InitializeComponent();
-            InitializeTimer();
-            LoadProcessorFamilyAsync();
+            
         }
 
-        private void InitializeTimer()
-        {
-            timer.Tick += Timer_Tick;
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Start();
-        }
-
-        private async void LoadProcessorFamilyAsync()
-        {
-            try
-            {
-                string processorFamily = await Task.Run(() => GetProcessorFamily());
-                Application.Current.Dispatcher.Invoke(() => LblProcFamily.Text = processorFamily);
-            }
-            catch (Exception ex)
-            {
-                // Handle the exception appropriately, such as logging or displaying an error message.
-                MessageBox.Show($"An error occurred while loading processor family: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-
-        private string GetProcessorFamily()
-        {
-            ManagementClass wmi = new ManagementClass("Win32_Processor");
-            var providers = wmi.GetInstances();
-            StringBuilder sbFamily = new StringBuilder();
-
-            foreach (var provider in providers)
-            {
-                int procFamily = Convert.ToInt16(provider["Family"]);
-
-                if (procFamily == 198)
-                {
-                    sbFamily.Append("Intel(R) Core(TM) i7");
-                }
-                else if (procFamily == 107)
-                {
-                    sbFamily.Append("AMD Ryzen 5 5600G");
-                }
-                else if (procFamily == 11)
-                {
-                    sbFamily.Append("Pentium(R) brand");
-                }
-            }
-
-            return sbFamily.ToString();
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            RAM.Value = (int)perfRAM.NextValue();
-            RAMpercent.Text = $"{RAM.Value}%";
-            LoadProcessorFamilyAsync();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            LogInWindow logIn = new LogInWindow();
-            logIn.Show();
-        }
+       
     }
 }
